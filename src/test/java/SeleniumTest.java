0dbbf42ad13435e737c4d0ff46b9083a227c4feb
partyhardeee <danseriv56@gmail.com>
2023-01-24 20:24:57 +0500

    Initial commit

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,19 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class SeleniumTest {
+    public static WebDriver driver;
+
+    @BeforeClass
+    public static void setup(){
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+
+    }
+
+    @Test
+    public void openBrowserTest() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.quit();
+    }
+}
